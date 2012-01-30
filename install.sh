a6576f017377440dfa90823ca40a225a96a7338d
Wang Yongjie <yongjiemail@gmail.com>
2012-01-30 17:30:34 +0800

    add purple config file for pidgin and install script for @dir

--- install.sh
+++ install.sh
@@ -1,4 +1,36 @@
 #!/usr/bin/env bash
+
+# link subfile of the dir start with @. 
+# like @abc/def/eg
+# link ~/.adc/def/eg -> @abc/def/eg
+function link_dir_file {
+    DIR=$1
+    if [ -d "$DIR" ]; then
+        for i in $DIR/*
+        do
+            link_dir_file $i
+        done
+    else
+        file=$DIR
+        source="${PWD}/$file"
+        target="${HOME}/${file/@/.}"
+        if [ -e "${target}" ]; then
+            mv $target $target.bak
+        else
+            mkdir -p $(dirname $target)
+        fi
+        ln -sf ${source} ${target}
+    fi
+    return
+}
+
+for i in @*
+do
+    link_dir_file $i
+done
+exit
+
+# 
 function link_file {
     source="${PWD}/$1"
     target="${HOME}/${1/_/.}"
